1b76e2ca488ded74388df4d6d1ed73fa56fefe9e
pablolimo <limopablo@gmail.com>
2019-08-26 02:02:23 -0500

    github pages

---
+++ build/precache-manifest.f85d9c96b409c984881640ebbb7cc786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733cf25a61fcdea8b2a02838311e8844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa37253c2d89b54279a",
+    "url": "/static/css/main.d1974727.chunk.css"
+  },
+  {
+    "revision": "0f515743437695146cea",
+    "url": "/static/js/2.aa7783f8.chunk.js"
+  },
+  {
+    "revision": "aaa37253c2d89b54279a",
+    "url": "/static/js/main.54988c40.chunk.js"
+  },
+  {
+    "revision": "a34755ca0d3d3dc01eca",
+    "url": "/static/js/runtime~main.21993c1e.js"
+  }
+]);
